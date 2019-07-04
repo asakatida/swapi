990652815403655883cd1b680dc58c1ec2cd87e9
Asa Katida <github@holomaplefeline.net>
2019-07-04 15:31:51 -0700

    reinit. (#256)

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,4 +1,15 @@
-// https://docs.cypress.io/guides/guides/plugins-guide.html
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
 
 module.exports = (on, config) => {
   return Object.assign({}, config, {
